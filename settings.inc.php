6a000108b0e1408a0a7185b180e3d40aa5d7aa5a
Jean Baptiste <marchetti.jb@gmail.com>
2015-09-25 19:14:08 +0200

    Use getenv in settings.inc

--- settings.inc.php
+++ settings.inc.php
@@ -1,8 +1,8 @@
 <?php
-define('_DB_SERVER_', $_ENV["DB_SERVER"]);
-define('_DB_NAME_', $_ENV["DB_NAME"]);
-define('_DB_USER_', $_ENV["DB_USER"]);
-define('_DB_PASSWD_', $_ENV["DB_PASSWD"]);
+define('_DB_SERVER_', getenv("DB_SERVER"));
+define('_DB_NAME_', getenv("DB_NAME"));
+define('_DB_USER_', getenv("DB_USER"));
+define('_DB_PASSWD_', getenv("DB_PASSWD"));
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
@@ -10,10 +10,10 @@ define('_PS_CACHE_ENABLED_', '0');
 define('_MEDIA_SERVER_1_', '');
 define('_MEDIA_SERVER_2_', '');
 define('_MEDIA_SERVER_3_', '');
-define('_COOKIE_KEY_', $_ENV["COOKIE_KEY"]);
-define('_COOKIE_IV_', $_ENV["COOKIE_IV"]);
-define('_PS_CREATION_DATE_', $_ENV["PS_CREATION_DATE"]);
-define('_PS_VERSION_', $_ENV["PS_VERSION"]);
-define('_RIJNDAEL_KEY_', $_ENV["RIJNDAEL_KEY"]);
-define('_RIJNDAEL_IV_', $_ENV["RIJNDAEL_IV"]);
+define('_COOKIE_KEY_', getenv("COOKIE_KEY"));
+define('_COOKIE_IV_', getenv("COOKIE_IV"));
+define('_PS_CREATION_DATE_', getenv("PS_CREATION_DATE"));
+define('_PS_VERSION_', getenv("PS_VERSION"));
+define('_RIJNDAEL_KEY_', getenv("RIJNDAEL_KEY"));
+define('_RIJNDAEL_IV_', getenv("RIJNDAEL_IV"));
 ?>

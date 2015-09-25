d76e4c8cee3ad3313ce8e6adca4a8528e068ccf2
Jean Baptiste <marchetti.jb@gmail.com>
2015-09-25 19:06:34 +0200

    Add env Variables in settings.inc

--- settings.inc.php
+++ settings.inc.php
@@ -1,3 +1,19 @@
 <?php
-  
+define('_DB_SERVER_', $_ENV["DB_SERVER"]);
+define('_DB_NAME_', $_ENV["DB_NAME"]);
+define('_DB_USER_', $_ENV["DB_USER"]);
+define('_DB_PASSWD_', $_ENV["DB_PASSWD"]);
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', $_ENV["COOKIE_KEY"]);
+define('_COOKIE_IV_', $_ENV["COOKIE_IV"]);
+define('_PS_CREATION_DATE_', $_ENV["PS_CREATION_DATE"]);
+define('_PS_VERSION_', $_ENV["PS_VERSION"]);
+define('_RIJNDAEL_KEY_', $_ENV["RIJNDAEL_KEY"]);
+define('_RIJNDAEL_IV_', $_ENV["RIJNDAEL_IV"]);
 ?>
